8b976708f91a57b7adae423919eb83d0a3f7dae5
Muhammad Qasim <72746qasim@gmail.com>
2025-05-08 10:46:44 +0500

    Login Frontend Completed

--- Inventory Management System/Frontend-InventoryManagementSystem/src/components/login-form/LoginForm.jsx
+++ Inventory Management System/Frontend-InventoryManagementSystem/src/components/login-form/LoginForm.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './LoginForm.css'; // Import your CSS file for custom styles
 
 
 const LoginForm = () => {
